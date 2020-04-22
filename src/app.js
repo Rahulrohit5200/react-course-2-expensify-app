fe202e4cea4a5371b373cabb62020a3913d72a3c
rahul <smartrj123@gmail.com>
2020-04-22 12:27:12 +0530

    Removing dummy expences data

--- src/app.js
+++ src/app.js
@@ -10,18 +10,7 @@ import {addexpence} from "./actions/expences";
 //import expenceReducer from "./reducers/expences";//noneed it will automatically go
 import getVisibleExpence from "./selector/expences";
 import { setTextFilter } from './actions/filters';
-const store=configurestore();//if we use some other name for store then we have to change in provider also {name of store} at down
-console.log(store.getState());
 
-
-store.dispatch(addexpence({description:"water bill",amount:500,createdAt:10000}));
-
-store.dispatch(addexpence({description:"gas bill",amount:1500,createdAt:1000}));
-store.dispatch(addexpence({description:"rent",amount:2500,createdAt:5000}));
-
-//store.dispatch(setTextFilter("bill"));
-const arr=getVisibleExpence(store.getState().expences,store.getState().filters);
-console.log(arr);
 
 const x=()=>{
     return (
